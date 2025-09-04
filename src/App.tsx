b2d685407817df7d156a2130e8626e8579f4980f
Pedro Henrique de Lima Malaquias <pedro.lima@finxapp.com.br>
2025-09-03 22:00:13 -0300

    inital settings

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Text from "./components/text";
+
+function App() {
+  return (
+    <div className="bg-color-pin">
+      <Text variant={"body-md"}>Here we go again! 👻</Text>
+    </div>
+  );
+}
+
+export default App;
